4a923257c81db91d02a31df4802b9bb6760436a6
adminvedatech <redilberto@gmail.com>
2020-03-05 15:26:25 -0600

    Pirmer commit

---
+++ src/test/java/com/vedatech/pro/VedatechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vedatech.pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VedatechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
